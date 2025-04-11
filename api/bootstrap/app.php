6377cbf86e674fd8ad9f662c29780e17919b11fa
belloLucas <lucasbellodev@gmail.com>
2025-04-10 22:15:39 -0300

    feat: registering middleware

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\ApiExceptionHandler;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api(ApiExceptionHandler::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
