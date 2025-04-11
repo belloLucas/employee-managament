52cd7ddc2dd8084a91288c45a1bcdac644f0d6f2
belloLucas <lucasbellodev@gmail.com>
2025-04-11 17:54:43 -0300

    refactor: updating routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -6,9 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/users', [UserController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
-    Route::apiResource('users', UserController::class);
+    Route::apiResource('users', UserController::class)->except(['index']);
 });
