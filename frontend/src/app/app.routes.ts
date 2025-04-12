e30797effab50d4d96891b3b30ce410d3108baad
belloLucas <lucasbellodev@gmail.com>
2025-04-11 22:53:55 -0300

    feat: creating routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './core/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { 
+    path: 'login', 
+    loadComponent: () => import('./login-page/login-page.component').then(m => m.LoginPageComponent)
+  },
+  { 
+    path: 'dashboard', 
+    loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent),
+    canActivate: [AuthGuard]
+  },
+  { 
+    path: 'employees/new', 
+    loadComponent: () => import('./employee-form/employee-form.component').then(m => m.EmployeeFormComponent),
+    canActivate: [AuthGuard]
+  },
+  { 
+    path: 'employees/:id', 
+    loadComponent: () => import('./employee-view/employee-view.component').then(m => m.EmployeeViewComponent),
+    canActivate: [AuthGuard]
+  },
+  { 
+    path: 'employees/:id/edit', 
+    loadComponent: () => import('./employee-form/employee-form.component').then(m => m.EmployeeFormComponent),
+    canActivate: [AuthGuard]
+  },
+  { path: '**', redirectTo: '/login' }
+];
