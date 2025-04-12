d98b136a8f82b50103e289e7f409539ee0bfd3cd
belloLucas <lucasbellodev@gmail.com>
2025-04-12 08:40:04 -0300

    refactor: updating employee service to handle correctly the api return object

--- frontend/src/app/services/employee.service.ts
+++ frontend/src/app/services/employee.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { Employee, EmployeeFormData } from '../models/employee.model';
 
 @Injectable({
@@ -12,19 +12,27 @@ export class EmployeeService {
   constructor(private http: HttpClient) {}
 
   getAllEmployees(): Observable<Employee[]> {
-    return this.http.get<Employee[]>(this.apiUrl);
+    return this.http.get<{employees: Employee[]}>(this.apiUrl).pipe(
+      map(response => response.employees)
+    );
   }
 
   getEmployeeById(id: number): Observable<Employee> {
-    return this.http.get<Employee>(`${this.apiUrl}/${id}`);
+    return this.http.get<{employee: Employee}>(`${this.apiUrl}/${id}`).pipe(
+      map(response => response.employee)
+    );
   }
 
   createEmployee(employee: EmployeeFormData): Observable<Employee> {
-    return this.http.post<Employee>(this.apiUrl, employee);
+    return this.http.post<{employee: Employee}>(this.apiUrl, employee).pipe(
+      map(response => response.employee)
+    );
   }
 
   updateEmployee(id: number, employee: EmployeeFormData): Observable<Employee> {
-    return this.http.put<Employee>(`${this.apiUrl}/${id}`, employee);
+    return this.http.put<{employee: Employee}>(`${this.apiUrl}/${id}`, employee).pipe(
+      map(response => response.employee)
+    );
   }
 
   deleteEmployee(id: number): Observable<void> {
