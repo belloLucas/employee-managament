826b5450c2b162eff341f0ffab5196e8224e3810
belloLucas <lucasbellodev@gmail.com>
2025-04-11 22:51:13 -0300

    refactor: updating app component to stop handling logic

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgIf } from '@angular/common';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { RegisterFormComponent } from './register-form/register-form.component';
 import { ToastComponent } from './toast/toast.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf, LoginFormComponent, RegisterFormComponent, ToastComponent],
+  imports: [RouterOutlet, ToastComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Employee Management';
-  authMode: 'login' | 'register' = 'login';
 }
